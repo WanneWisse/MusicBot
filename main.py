d6eee02270765ed53af5bcec46a307f6cfc4661c
Wanne Wisse <wanne.wisse@gmail.com>
2024-03-18 23:24:45 +0100

    Music bot logic

---
+++ main.py
@@ -0,0 +1,24 @@
+import discord
+from discord.ext import commands
+import discord
+from discord.ext import commands
+from bot import Music
+import configparser
+
+# Bot prefix
+BOT_PREFIX = "!"
+
+
+intents = discord.Intents().all()
+# Create a bot instance
+bot = commands.Bot(command_prefix=BOT_PREFIX,intents=intents)
+# Event: Bot is ready
+@bot.event
+async def on_ready():
+    print(f'{bot.user.name} has connected to Discord!')
+    await bot.add_cog(Music(bot))
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+bot.run(config.get('Bot', 'token'))
+
