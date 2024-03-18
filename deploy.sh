72566f415377c6ee96cf0d8fcabc85205203f369
Wanne Wisse <wanne.wisse@gmail.com>
2024-03-19 00:10:51 +0100

    deploy folder

--- deploy.sh
+++ deploy.sh
@@ -2,9 +2,9 @@
 pkill -x "java"
 pkill -x "python"
 
-nohup "java -jar Lavalink.jar" &
+nohup "java -jar /home/ubuntu/musicBot/MusicBot/Lavalink.jar" &
 
 source "/home/ubuntu/musicBot/env/bin/activate"
-nohup "python main.py" &
+nohup "python /home/ubuntu/musicBot/MusicBot/main.py" &
 
 
