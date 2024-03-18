6ce31b19ada4fb8a351b2a69889fd089ae10d10c
Wanne Wisse <wanne.wisse@gmail.com>
2024-03-19 00:34:37 +0100

    prevent overflowing deploy

--- deploy.sh
+++ deploy.sh
@@ -2,10 +2,10 @@
 pkill -x "java"
 pkill -x "python"
 
-nohup java -jar /home/ubuntu/musicBot/MusicBot/Lavalink.jar &
+nohup java -jar /home/ubuntu/musicBot/MusicBot/Lavalink.jar >/dev/null 2>&1 &
 
 source /home/ubuntu/musicBot/env/bin/activate
 
-nohup python /home/ubuntu/musicBot/MusicBot/main.py &
+nohup python /home/ubuntu/musicBot/MusicBot/main.py >/dev/null 2>&1 &
 
 
