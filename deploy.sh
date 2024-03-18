2068c6fc1a2e18f306076081a3ce8d877b9e54e0
Wanne Wisse <wanne.wisse@gmail.com>
2024-03-19 00:25:27 +0100

    changes to deployer

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,7 @@ pkill -x "python"
 nohup "java -jar /home/ubuntu/musicBot/MusicBot/Lavalink.jar" &
 
 source "/home/ubuntu/musicBot/env/bin/activate"
+
 nohup "python /home/ubuntu/musicBot/MusicBot/main.py" &
 
 
