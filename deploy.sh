833124516c02c892b6e3c90b71001ddeff8dd1d0
Wanne Wisse <wanne.wisse@gmail.com>
2024-03-28 22:42:52 +0100

    deloy latestest

--- deploy.sh
+++ deploy.sh
@@ -2,10 +2,10 @@
 pkill -x "java"
 pkill -x "python"
 
-nohup java -jar /home/ubuntu/musicBot/MusicBot/Lavalink.jar >/dev/null 2>&1 &
+nohup java -jar /home/ubuntu/musicBot/MusicBot/Lavalink.jar >/home/ubuntu/musicBot/MusicBot/output_java.txt 2>&1 &
 
 source /home/ubuntu/musicBot/env/bin/activate
 
-nohup python /home/ubuntu/musicBot/MusicBot/main.py >/dev/null 2>&1 &
+nohup python /home/ubuntu/musicBot/MusicBot/main.py >/home/ubuntu/musicBot/MusicBot/output_python.txt 2>&1 &
 
 
